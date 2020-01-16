25033b4348ec5e5709aa9cab041da774cf6bcabd
J. Ayo Akinyele <ayo@boltlabs.io>
2020-01-16 00:47:37 -0500

    clean up and add new conn type

--- tokens/tokens.h
+++ tokens/tokens.h
@@ -11,8 +11,9 @@ extern "C" {
  * - CUSTOM = custom connection handle defined by caller
  * - NETIO = tcp socket on localhost
  * - UNIXNETIO = unix domain socket with a specified file
+ * - TORNETIO = onion-routing based connection
  */
-enum ConnType { CUSTOM = 0, NETIO, UNIXNETIO };
+enum ConnType { CUSTOM = 0, NETIO, UNIXNETIO, TORNETIO };
 
 /* IO Channel Callback interface - allows caller to handle
  * how connection is made between customer and merchant
@@ -21,11 +22,9 @@ typedef void* (*IOCallback)(ConnType c, int party);
 
 /* Returns a pointer to a NetIO ptr */
 void* get_netio_ptr(char *address, int port, int party);
-//void free_netio_ptr(void *io_ptr);
 
 /* Returns a pointer to a UnixNetIO ptr */
 void* get_unixnetio_ptr(char *socket_path, int party);
-//void free_unixnetio_ptr(void *io);
 
 /*
  * describes an API for calling MPC functions 
