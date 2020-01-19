b27a30189c23366ac36fed34fd728555a20dda51
Marcella Hastings <marcellahastings@gmail.com>
2020-01-19 18:30:37 -0500

    add framework for ecdsa e2e tests

---
+++ tokens/test.h
@@ -0,0 +1,14 @@
+#ifndef TEST_INCLUDE_H_
+#define TEST_INCLUDE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#include <stdint.h>
+#endif
+
+void build_test();
+
+#ifdef __cplusplus
+}
+#endif
+#endif // TEST_INCLUDE_H_
