d2887d4d6e4d0ed2f8bdfcfb17626ee50ba46c8d
Marcella Hastings <marcellahastings@gmail.com>
2020-01-20 15:48:45 -0500

    update ecdsa testing api

--- tokens/test.cpp
+++ tokens/test.cpp
@@ -1,4 +1,24 @@
 #include "test.h"
+#include<iostream>
+using namespace std;
 
-void build_test() {
+void test_ecdsa_e2e(EcdsaPartialSig_l psl, uint32_t digest[8]) {
+
+  // also does nothing
+  cout << "here we are doing something wooo" << endl;
+
+  // format partial signature
+  /*
+  EcdsaPartialSig_l psl;
+  fillEcdsaPartialSig_l(&psl, r, k_inv);
+  EcdsaPartialSig_d psd = distribute_EcdsaPartialSig(psl);
+
+  // compute and parse result
+  string actual = ecdsa_sign_hashed(e, psd).reveal_unsigned(PUBLIC);
+  string myfull = r + actual;
+  actual = change_base(actual, 10, 16);
+  while (actual.length() < 64) {
+    actual = '0' + actual;
+  }
+  */
 }
